97d43d6aa1c6147787db06ea58c80f90c8eabdb1
mohammad-nayem <nishanahmed13913@gmail.com>
2024-12-04 17:06:25 +0600

    Make a sili changes in UserDataValidation Utilit file

--- app/utility/UserDataValidation.js
+++ app/utility/UserDataValidation.js
@@ -4,6 +4,7 @@ let saltRounds  = 10;
 // Checking for data validation
 export const UserData = async (reqBody) => {
     const {email, firstName, lastName, mobile, password} = reqBody;
+    
     if(email && firstName &&  lastName && mobile && password) { 
         const hashedPassword = await bcrypt.hash(password, saltRounds);
        
